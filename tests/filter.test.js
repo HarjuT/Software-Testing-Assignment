341910fd15216f6f497025a763fb530d2b6ad856
Teemu Harju <teemu.j.harju@tuni.fi>
2022-12-07 14:31:39 +0200

    Unit test for filter function completed.

--- tests/filter.test.js
+++ tests/filter.test.js
@@ -8,15 +8,38 @@ describe("filter", function() {
         expect(filter(users, function(){})).to.eql([[]]);
     });
 
-    it("should return true when values are equivalent", function() {
-        
+    it("should return array", function() {
+        const users = 
+        [
+            { 'user': 'barney', 'active': true },
+            { 'user': 'fred',   'active': false }
+        ]
+        expect(filter(users, ({ active }) => active)).to.be.an('array')
     });
 
-    it("should return false when values are not equivalent", function() {
-       
+    it("should return array with the correct item", function() {
+        const users = 
+        [
+            { 'user': 'barney', 'active': true },
+            { 'user': 'fred',   'active': false }
+        ]
+        expect(filter(users, ({ active }) => active)).to.have.deep.members([{ 'user': 'barney', 'active': true }])
     });
 
-    it("should return true when values are not equivalent", function() {
-        
+    it("should return array with the correct items", function() {
+        const users = 
+        [
+            { 'user': 'barney', 'active': true },
+            { 'user': 'fred',   'active': false },
+            { 'user': 'harry',  'active': true },
+            { 'user': 'jack',   'active': true }
+        ]
+        expect(filter(users, ({ active }) => active)).to.have.deep.members(
+            [
+                { 'user': 'barney', 'active': true },
+                { 'user': 'harry',  'active': true },
+                { 'user': 'jack',   'active': true }
+            ]
+        )
     });
 });
