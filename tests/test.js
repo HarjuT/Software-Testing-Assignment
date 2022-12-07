c03fe05db2568e9ea8bf1d1ed15a6d5901313c6f
Teemu Harju <teemu.j.harju@tuni.fi>
2022-12-07 13:33:15 +0200

    difference test made and needed functions added.

--- tests/test.js
+++
@@ -1,8 +0,0 @@
-//const expect = require('chai').expect;
-import { expect } from "chai";
-
-describe("Test", function() {
-    it("1+2=2", function() {
-        expect(1+1).to.equal(2)
-    });
-});
